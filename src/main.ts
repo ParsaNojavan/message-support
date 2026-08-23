35d0023da0d78a365dff56429ddf0cd7566edb31
parsanojavan <parsa.nojavan85@gmail.com>
2026-08-24 00:42:53 +0330

    feat(support): add support gateway and auth

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SupportModule } from './support.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(SupportModule);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
